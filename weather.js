9fa800a93891638eb115df90aeac5a0cb330037e
Stefan Knežević <40828405+StefanKnezevic@users.noreply.github.com>
2019-05-23 12:37:24 +0200

    Update weather.js

--- weather.js
+++ weather.js
@@ -5,7 +5,7 @@ class Weather {
     this.state = state
   }
   async getWeather() {
-     const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${this.city},${this.state}&units=metric&APPID=${this.apiKey}`)
+     const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${this.city},${this.state}&units=metric&APPID=${this.apiKey}`)
     const resData = await response.json()
     return resData
   }
